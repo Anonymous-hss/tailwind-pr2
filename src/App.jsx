4a89cf51bd506b8a402aa2cfe8fd49eeac6249b6
Anonymous-hss <harshalsawatkar24@gmail.com>
2025-01-05 17:33:03 +0530

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { SidebarClass } from "./Components/sidebar";
-
 function App() {
   return (
-    <div>
-      <SidebarClass></SidebarClass>
+    <div className="h-screen bg-white dark:bg-blue-800 text-black dark:text-white">
+      <h1 className="text-black dark:text-black">Hello there </h1>
+      <button
+        onClick={() => {
+          document.querySelector("html").classList.toggle("dark");
+        }}
+      ></button>
     </div>
   );
 }
--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -1,7 +1,7 @@
 export function SidebarClass() {
   return (
     <div className="flex">
-      <div className="transition-all delay-1000 bg-red-200  md:w-96 w-0 h-screen">
+      <div className="transition-all ease-in-out delay-2000 duration-1000 bg-red-200  md:w-96 w-0 h-screen">
         Sidebar
       </div>
       <div className="bg-green-800 w-full">Content</div>
